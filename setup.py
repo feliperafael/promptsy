44c219ccabe9c4832aa4de61920d06a9987f778e
Felipe Rafael <feliperafael_95@yahoo.com.br>
2024-09-10 21:07:52 -0300

    add data in package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='promptsy',
-    version='0.1.3',
+    version='0.1.3.2',
     description='A Python library for managing and organizing prompts for Large Language Models (LLMs)',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -10,6 +10,10 @@ setup(
     author_email='felipe@evakub.com.br',
     url='https://github.com/feliperafael/promptsy',
     packages=find_packages(),
+    include_package_data=True,  # Inclui arquivos de dados
+    package_data={
+        'promptsy': ['prompts/*.yaml'],  # Inclui todos os arquivos YAML na pasta prompts
+    },
     install_requires=[
         'PyYAML',
         'colorama',
